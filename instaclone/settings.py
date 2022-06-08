fa01f27bf9c71eda537c0199e3901111d21275bd
annk1995 <kabuikariuki052@gmail.com>
2022-06-09 00:15:01 +0300

    corrected errors

--- instaclone/settings.py
+++ instaclone/settings.py
@@ -70,7 +70,7 @@ WSGI_APPLICATION = 'instaclone.wsgi.application'
 # DATABASES = {
 #     'default': {
 #         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': 'mydatabase',
+
 
 
 MODE=config("MODE", default="dev")
